9ad282b045fe414e7b9c68de3da0a29ed69d7764
Moksh Keloo <moksh@integratedwebworks.com>
2024-12-16 18:09:31 -0500

    Setting up routes and all. Onto Onboarding Screens now.

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import {SafeAreaView} from "react-native-safe-area-context";
+
+const SignUp = () => {
+    return (
+        <SafeAreaView className="flex items-center justify-center h-screen bg-white">
+            <View className="flex items-center justify-center">
+                <Text className="font-bold text-blue-600 text-2xl">SignUp Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default SignUp;
